9d35d4591f3cca45de28adb3eb0c90627a8b15a1
lovieira <luk14236@gmail.com>
2016-11-29 00:20:56 -0200

    enviando a conclusão da escolha do template

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Bairro)
+admin.site.register(Empresa)
+admin.site.register(Repasse)
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(SubCategoria)
+admin.site.register(Produto)
+admin.site.register(Foto)
+admin.site.register(Pedido)
+admin.site.register(Item)
--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Estado:
+class Estado(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=256)
 
@@ -15,7 +15,7 @@ class Estado:
         return str(self.nome)
 
 
-class Cidade:
+class Cidade(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=256)
     estado = models.ForeignKey("Estado", related_name="cidades")
@@ -28,7 +28,7 @@ class Cidade:
         return str(self.nome)
 
 
-class Bairro:
+class Bairro(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=256)
     cidade = models.ForeignKey("Cidade", related_name="bairros")
@@ -41,7 +41,7 @@ class Bairro:
         return str(self.nome)
 
 
-class Empresa:
+class Empresa(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=256)
     descricao = models.CharField(max_length=1024)
@@ -57,13 +57,14 @@ class Empresa:
         return str(self.nome)
 
 
-class Cobranca:
+class Repasse(models.Model):
     id = models.AutoField(primary_key=True)
     referencia = models.CharField(max_length=512)
     data_inicio = models.DateField(auto_now=True)
     data_fim = models.DateField(null=True)
     total = models.FloatField()
     pago = models.BooleanField(default=False)
+    empresa = models.ForeignKey("Empresa", related_name="Repasses")
 
     class Meta:
         verbose_name = "Cobrança"
@@ -73,7 +74,7 @@ class Cobranca:
         return str(self.referencia)
 
 
-class Cliente:
+class Cliente(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=512)
     email = models.CharField(max_length=512)
@@ -91,5 +92,82 @@ class Cliente:
         return str(self.nome)
 
 
-# class Cardapio:
-#     id = models
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __unicode__(self):
+        return str(self.nome)
+
+
+class SubCategoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=256)
+    categoria = models.ForeignKey("Categoria", related_name="categoria")
+
+    class Meta:
+        verbose_name = "Subcategoria"
+        verbose_name_plural = "SubCategorias"
+
+    def __unicode__(self):
+        return str(self.nome)
+
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=512)
+    foto = models.CharField(max_length=1024)
+    descricao = models.CharField(max_length=1024)
+    preco = models.FloatField(null=True)
+
+    class Meta:
+        verbose_name="Produto"
+        verbose_name_plural="Produtos"
+
+    def __unicode__(self):
+        return str(self.nome)
+
+
+class Foto(models.Model):
+    id = models.AutoField(primary_key=True)
+    caminho = models.CharField(max_length=1024)
+    produto = models.ForeignKey("Produto", related_name="fotos")
+
+    class Meta:
+        verbose_name = "Foto"
+        verbose_name_plural = "Fotos"
+
+    def __unicode__(self):
+        return self.produto.nome + "_" + str(self.id)
+
+
+class Pedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    data = models.DateField(auto_now=True)
+    total = models.FloatField()
+    cliente = models.ForeignKey("Cliente", related_name="Pedidos")
+
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+
+    def __unicode__(self):
+        return self.cliente.nome + "_" + str(self.id)
+
+
+class Item(models.Model):
+    id=models.AutoField(primary_key=True)
+    pedido=models.ForeignKey("Pedido", related_name="Itens")
+    quantidade=models.IntegerField(default=1)
+    produto = models.ForeignKey("Produto")
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Itens"
+
+    def __unicode__(self):
+        return self.produto.nome + "_" + str(self.id)
