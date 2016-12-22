bffea53e72b72269eb6cd14cbc341d86c2920e2a
lovieira <luk14236@gmail.com>
2016-12-22 00:25:35 -0200

    enviando a continuacão dos itens de url

--- game7api/urls.py
+++ game7api/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from core.views import *
 
 urlpatterns = [
+    url(r'^$', adminHomeView.as_view(), name="Home"),
+
     url(r'^admin/', admin.site.urls),
     url(r'^adm/login/', adminLoginView.as_view(), name = "Login"),
     url(r'^adm/home/', adminHomeView.as_view(), name = "Home-adm"),
@@ -27,16 +29,55 @@ urlpatterns = [
     url(r'^adm/categoria-excluir$', adminCategoriaExcluirView.as_view(), name = "ExcluirCategoria-adm"),
     url(r'^adm/categoria-ver', adminCategoriaVerView.as_view(), name="VerCategoria-adm"),
     url(r'^adm/categoria-edicao', adminCategoriaEdicaoView.as_view(), name="EdicaoCategoria-adm"),
-    url(r'^adm/bd-categoria-excluir$', dbadmincategoriaexcluir, name = "BDExcluirCategoria-adm"),
 
     url(r'^adm/subcategorias/', adminSubCategoriasView.as_view(), name="SubCategorias-adm"),
     url(r'^adm/subcategorias-nova', adminSubCategoriaNovaView.as_view(), name="NovaSubCategoria-adm"),
     url(r'^adm/subcategoria-excluir$', adminSubCategoriaExcluirView.as_view(), name="ExcluirSubCategoria-adm"),
     url(r'^adm/subcategoria-ver', adminSubCategoriaVerView.as_view(), name="VerSubCategoria-adm"),
     url(r'^adm/subcategoria-edicao', adminSubCategoriaEdicaoView.as_view(), name="EdicaoSubCategoria-adm"),
-    url(r'^adm/bd-subcategoria-excluir$', dbadminsubcategoriaexcluir, name = "BDExcluirSubCategoria-adm"),
 
     url(r'^adm/clientes/', adminClientesView.as_view(), name="Clientes-adm"),
     url(r'^adm/clientes-nova/', adminClientesNovaView.as_view(), name="NovoCliente-adm"),
-    url(r'^$', adminHomeView.as_view(), name = "Home"),
+
+    url(r'^js/categorias', ServiceJson.categorias, name='categorias'),
+    url(r'^js/clientes', ServiceJson.clientes, name='clientes'),
+    url(r'^js/subcategorias', ServiceJson.subcategorias, name='subcategorias'),
+    url(r'^js/estados', ServiceJson.estados, name='estados'),
+    url(r'^js/cidades', ServiceJson.cidades, name='Cidades'),
+    url(r'^js/bairros', ServiceJson.bairros, name='bairros'),
+    url(r'^js/empresas', ServiceJson.empresas, name='empresas'),
+    url(r'^js/funcionarios', ServiceJson.funcionarios, name='funcionarios'),
+    url(r'^js/Repasses', ServiceJson.Repasses, name='Repasses'),
+    url(r'^js/produtos', ServiceJson.produtos, name='produtos'),
+    url(r'^js/fotos', ServiceJson.fotos, name='fotos'),
+    url(r'^js/pedidos', ServiceJson.pedidos, name='pedidos'),
+    url(r'^js/itens', ServiceJson.itens, name='itens'),
+
+    url(r'^js/savecategorias', ServiceJson.savecategoria, name='savecategoria'),
+    url(r'^js/savecliente', ServiceJson.savecliente, name='savecliente'),
+    url(r'^js/savesubcategoria', ServiceJson.savesubcategoria, name='savesubcategoria'),
+    url(r'^js/saveestado', ServiceJson.saveestado, name='saveestado'),
+    url(r'^js/savecidade', ServiceJson.savecidade, name='savecidade'),
+    url(r'^js/savebairro', ServiceJson.savebairro, name='savebairro'),
+    url(r'^js/saveempresa', ServiceJson.saveempresa, name='saveempresa'),
+    url(r'^js/saveempresa_bairro', ServiceJson.saveempresa_bairro, name='saveempresa_bairro'),
+    url(r'^js/savefuncionario', ServiceJson.savefuncionario, name='savefuncionario'),
+    url(r'^js/saverepasse', ServiceJson.saverepasse, name='saverepasse'),
+    url(r'^js/saverproduto', ServiceJson.saverproduto, name='saverproduto'),
+    url(r'^js/savefoto', ServiceJson.savefoto, name='savefoto'),
+    url(r'^js/savepedido', ServiceJson.savepedido, name='savepedido'),
+    url(r'^js/saveitem', ServiceJson.saveitem, name='saveitem'),
+
+    url(r'^js/excluircategoria', ServiceJson.excluircategoria, name='excluircategoria'),
+    url(r'^js/excluircliente', ServiceJson.excluircliente, name='excluircliente'),
+    url(r'^js/excluirsubcategoria', ServiceJson.excluirsubcategoria, name='excluirsubcategoria'),
+    url(r'^js/excluirempresa', ServiceJson.excluirempresa, name='excluirempresa'),
+    url(r'^js/excluirempresa_bairro', ServiceJson.excluirempresa_bairro, name='excluirempresa_bairro'),
+    url(r'^js/excluirfuncionario', ServiceJson.excluirfuncionario, name='excluirfuncionario'),
+    url(r'^js/excluirrepasse', ServiceJson.excluirrepasse, name='excluirrepasse'),
+    url(r'^js/excluirproduto', ServiceJson.excluirproduto, name='excluirproduto'),
+    url(r'^js/excluirfoto', ServiceJson.excluirfoto, name='excluirfoto'),
+    url(r'^js/excluirpedido', ServiceJson.excluirpedido, name='excluirpedido'),
+    url(r'^js/excluiritem', ServiceJson.excluiritem, name='excluiritem'),
+
 ]
